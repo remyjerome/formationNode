e00a06263cddfaa5696f60b5a9fa25c10a92a662
remyjerome <remy.jerome@live.com>
2020-12-17 18:15:47 +0100

    promisify

--- src/home/home.route.js
+++ src/home/home.route.js
@@ -1,11 +1,21 @@
-module.exports = function setHomeRoute(app){
-  app.get('/', (req, res) => {
-    res.render('home.twig', {
-      message: 'Hello world'
-    });
-  });
-    
+const { promisify } = require('util')
+
+const Twig = require('twig')
+
+const renderFilePromisified = promisify(Twig.renderFile)
+
+module.exports = function setHomeRoute(app) {
+  app.get('/', async (req, res) => {
+
+    try {
+      const html = await renderFilePromisified('./src/home/home.twig', { message: 'bar' });
+      res.send(html)
+    } catch (err) {
+      console.error(err)
+    }
+  })
+
   app.get('/coucou', (req, res) => {
     res.send('Orange!');
-  });
-};
+  })
+}
