46049cdecbcc87fc89acc8074fb0f0d45c6d847e
remyjerome <remy.jerome@live.com>
2020-12-18 14:51:15 +0100

    redis config

--- src/login/login.route.js
+++ src/login/login.route.js
@@ -1,9 +1,21 @@
 const { promisify } = require('util');
+const redis = require("redis");
 
 const Twig = require('twig');
-const renderFilePromisified = promisify(Twig.renderFile);
 
-module.exports = function setLoginRoute(app) {
+const client = redis.createClient(process.env.REDIS_URL, {
+  no_ready_check: true
+});
+
+const renderFilePromisified = promisify(Twig.renderFile);
+const getPromisfied = promisify(client.get).bind(client);
+const setPromisfied = promisify(client.set).bind(client);
+
+client.on("error", function (error) {
+  console.error(error);
+});
+
+module.exports = async function setLoginRoute(app) {
   app.get('/login', async (req, res) => {
     try {
       const html = await renderFilePromisified('./src/login/login.twig');
@@ -19,6 +31,10 @@ module.exports = function setLoginRoute(app) {
     }
 
     const json = JSON.stringify({ name: req.body.name });
+
+    await setPromisfied('user', json)
+
+    getPromisfied()
 
     res.cookie('currentUser', json, { maxAge: 1000 * 60 * 60 });
 
